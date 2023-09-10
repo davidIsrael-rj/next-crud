9563542c4d1a6f20aed4ffe493f5f9af09091073
DavidIsrael <davidisrael.rj@gmail.com>
2023-09-10 19:08:19 -0300

    Componente Tabela #03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,17 +14,28 @@ export default function Home() {
     new Cliente('Carla', 25, '3'),
     new Cliente('Pedro', 55, '4'),
   ]
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente){
+    console.log(`Excluir...${cliente.nome}`)
+  }
+
   return (
-   
-      <div className={`
+
+    <div className={`
         flex h-screen justify-center items-center
         bg-gradient-to-r from-purple-500 to-blue-600
         text-white
       `}>
-        <Layout titulo='Cadastro Simples'>
-          <Tabela clientes={clientes}></Tabela>
-        </Layout>
-      </div>
+      <Layout titulo='Cadastro Simples'>
+        <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}
+        />
+      </Layout>
+    </div>
 
-     )
+  )
 }
