32475d2acac20de9b200997955d2209542047612
DavidIsrael <davidisrael.rj@gmail.com>
2023-09-11 14:02:49 -0300

    Componente Formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
 import Botao from '../components/Botao'
+import Formulario from '../components/Formulario'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,10 +35,11 @@ export default function Home() {
         <div className='flex justify-end'>
           <Botao cor="green" className='mb-4'>Novo Cliente</Botao>
         </div>
-        <Tabela clientes={clientes}
+        <Formulario cliente={clientes[0]}/>
+        {/* <Tabela clientes={clientes}
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
-        />
+        /> */}
       </Layout>
     </div>
 
