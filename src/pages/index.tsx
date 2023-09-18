e215550eb7f2799299394fd72c6885eeb51b7ab5
DavidIsrael <davidisrael.rj@gmail.com>
2023-09-17 21:17:49 -0300

    Integrando Cadastro com Firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,39 +5,47 @@ import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
 import Botao from '../components/Botao'
 import Formulario from '../components/Formulario'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ClienteRepositorio from '../core/ClienteRepositorio'
+import ColecaoCliente from '../backend/db/ColecaoCliente'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  
+
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carla', 25, '3'),
-    new Cliente('Pedro', 55, '4'),
-  ]
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
   function clienteSelecionado(cliente: Cliente) {
     // console.log(cliente.nome)
     setCliente(cliente)
     setVisivel('form')
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir...${cliente.nome}`)
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
-  function novoCliente(){
+  function novoCliente() {
     setCliente(Cliente.vazio())
     setVisivel('form')
   }
 
-  function salvarCliente(cliente: Cliente){
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
 
@@ -64,10 +72,10 @@ export default function Home() {
           </>
 
         ) : (
-          <Formulario 
-          cliente={cliente}
-          clienteMudou={salvarCliente}
-          cancelado={()=> setVisivel('tabela')}
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel('tabela')}
           />
 
         )}
